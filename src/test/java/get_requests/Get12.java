60ca71ee63ce0b2502a1632731c70eaa8732a482
Burak Öz <burakoz2006@gmail.com>
2022-11-03 22:22:13 +0300

    nov3

---
+++ src/test/java/get_requests/Get12.java
@@ -0,0 +1,40 @@
+package get_requests;
+
+import org.junit.Test;
+
+public class Get12 {
+     /*
+    Given
+      1) https://jsonplaceholder.typicode.com/todos
+      2)
+   When
+       I send POST Request to the Url
+   Then
+       Status code is 201 {
+            "userId": 55,
+            "title": "Tidy your room",
+            "completed": false
+                         }
+   And
+       response body is like {
+                               "userId": 55,
+                               "title": "Tidy your room",
+                               "completed": false,
+                               "id": 201
+                               }
+*/
+
+    @Test
+    public void get12() {
+
+        //1. Set The URL
+
+        // 2. Set The Expected Data ( put, post, patch)
+
+        // 3. Send The Request And Get The Response
+
+        // 4. Do Assertion
+
+
+    }
+}
--- src/test/java/test_data/GoRestTestData.java
+++ src/test/java/test_data/GoRestTestData.java
@@ -18,6 +18,7 @@ public class GoRestTestData {
     public Map<String,Object> expectedDataMethod(Object meta, Map<String,String> data){
         Map<String,Object> expectedData = new HashMap<>();
         expectedData.put("meta",meta);
+
         expectedData.put("data",data);
 
         return expectedData;
