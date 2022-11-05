493717aef125dc98d97aafa9c8bd1dfb6ae1ca1d
Burak Öz <burakoz2006@gmail.com>
2022-11-05 20:01:13 +0300

    nov5

--- src/test/java/put_requests/Put01.java
+++ src/test/java/put_requests/Put01.java
@@ -1,7 +1,16 @@
 package put_requests;
 
 import base_urls.JsonplaceholderBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.Test;
+import test_data.JsonPlaceHolderTestData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class Put01 extends JsonplaceholderBaseUrl {
     /*
@@ -26,12 +35,23 @@ I send PUT Request to the Url
     @Test
     public void put01() {
         //1. Set The URL
+        spec.pathParams("first","todos","second",198);
 
         // 2. Set The Expected Data ( put, post, patch)
+        JsonPlaceHolderTestData obj = new JsonPlaceHolderTestData();
+        Map<String,Object> expectedData = obj.expectedDataMethod(21,"Wash the dishes",false);
+        System.out.println("expectedData = " + expectedData);
 
         // 3. Send The Request And Get The Response
+        Response response = given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().put("/{first}/{second}");
+        response.prettyPrint();
 
         // 4. Do Assertion
+        Map<String,Object> actualData =  response.as(HashMap.class);
+        System.out.println("actualData = " + actualData);
 
+        assertEquals(expectedData.get("userId"),actualData.get("userId"));
+        assertEquals(expectedData.get("title"),actualData.get("title"));
+        assertEquals(expectedData.get("completed"),actualData.get("completed"));
     }
 }
